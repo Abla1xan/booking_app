c0df91e215299a11e9a3256fb04d6ab0335b824d
Abla1xan <abylaykhan2006@gmail.com>
2026-05-13 17:55:01 +0500

    Редактор записи 2,0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3527160965" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4157995815" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
